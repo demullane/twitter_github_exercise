9f7617da7f62e5d2ec3e38a3df77cc3c91694eb5
Danielle <danielle@daniellemullane.com>
2015-03-10 14:12:12 -0600

    First commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,6 @@ class HomeController < ApplicationController
   def index
     @tweet_fetcher = TweetFetcher.new
     @tweet_finder = @tweet_fetcher.search_twitter(params[:username])
-    puts "HELLO"*6
-    puts params[:username]
   end
 
 end
